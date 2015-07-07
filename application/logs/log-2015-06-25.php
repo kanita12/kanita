da3838ab914ab78d6d73de5749e6ff7f5b264d12
KanexKane <kanexkane@gmail.com>
2015-07-07 12:23:14 +0700

    Delete Log

--- application/logs/log-2015-06-25.php
+++
@@ -1,5 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-
-ERROR - 2015-06-25 12:33:39 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: NO) /Applications/MAMP/htdocs/hrsystem/system/database/drivers/mysqli/mysqli_driver.php 135
-ERROR - 2015-06-25 12:33:39 --> Unable to connect to the database
-ERROR - 2015-06-25 12:33:39 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /Applications/MAMP/htdocs/hrsystem/system/core/Exceptions.php:272) /Applications/MAMP/htdocs/hrsystem/system/core/Common.php 569
